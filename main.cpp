8735ddb9c47c3f98c7a237faf73dbeaf17d4d80a
KashinaYV <Kashina@exmaple.com>
2021-09-06 22:25:53 +0300

    code: добавлен заголовчный файл с расширением h и файл с расширением cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -14,23 +15,6 @@ input_numbers(size_t count)
     return result;
 }
 
-void
-find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers [0];
-    for (double number : numbers)
-        {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-         {
-            max = number;
-         }
-        }
-}
 
 vector<size_t>
 make_histogram (const vector<double>& numbers,size_t bin_count)
@@ -105,14 +89,14 @@ svg_end()
 void
 svg_text(double left, double baseline , string text)
 {
-    cout << "<text x='" << left << "' y='" << baseline << "'>"<<text<<"</text>";
+    cout << "<text x='" << left << "' y='" << baseline << "'>"<<text<<"</text>"<<endl;
 }
 
 void
 svg_rect(double x, double y, double width, double height,
          string stroke= "red" , string fill= "#ffeeee" )
 {
-    cout << "<rect x='" << x << "' y='" << y <<"' width='"<< width <<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+    cout << "<rect x='" << x << "' y='" << y <<"' width='"<< width <<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />"<<endl;
 }
 
 
