c12dd6f05d9c6a79d05da36559fe8cce396e7af1
KashinaYV <Kashina@exmaple.com>
2021-09-06 22:57:30 +0300

    code: функции перенесы в файл реализации, также заигнорированны depend и layout

--- histogram.h
+++ histogram.h
@@ -8,4 +8,10 @@ using namespace std;
 void
 find_minmax(const vector<double>& numbers, double& min, double& max);
 
+vector<size_t>
+make_histogram (const vector<double>& numbers,size_t bin_count);
+
+vector<size_t>
+make_histogram (const vector<double>& numbers,size_t bin_count);
+
 #endif // HISTOGRAM_H_INCLUDED
