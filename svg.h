e127ae80d5b198f208ac5d1fba3a8b65fe700ce9
KashinaYV <Kashina@exmaple.com>
2021-09-10 02:14:46 +0300

    code: Сохранение данных из сети в буфер

--- svg.h
+++ svg.h
@@ -17,7 +17,6 @@ svg_text(double left, double baseline , string text);
 void
 svg_rect(double x, double y, double width, double height,string stroke= "red" , string fill= "#ffeeee" );
 
-void
-show_histogram_svg(const vector<size_t>& bins);
+void show_histogram_svg(const vector<size_t>& bins, size_t& bin_height);
 
 #endif // SVG_H_INCLUDED
