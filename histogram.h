e261cf4663df788e044e83b289a171e89cdb62c0
KashinaYV <Kashina@exmaple.com>
2021-09-09 23:08:37 +0300

    code: Упорядочение передачи данных по программе ( дописан read_input, переписана make_histogram

--- histogram.h
+++ histogram.h
@@ -5,13 +5,15 @@
 
 using namespace std;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 void
 find_minmax(const vector<double>& numbers, double& min, double& max);
 
 vector<size_t>
-make_histogram (const vector<double>& numbers,size_t bin_count);
-
-vector<size_t>
-make_histogram (const vector<double>& numbers,size_t bin_count);
+make_histogram (const Input& data);
 
 #endif // HISTOGRAM_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+
 vector<double>
 input_numbers(istream& in,size_t count)
 {
@@ -16,23 +17,26 @@ input_numbers(istream& in,size_t count)
     return result;
 }
 
+Input
+read_input(istream& in) {
+    Input data;
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+    cerr <<"Enter bin count:";
+    in>>data.bin_count;
+
+    return data;
+}
+
 
 int
 main() {
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-
-    cerr << "Enter numbers: ";
-    const auto numbers=input_numbers(cin,number_count);
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-    double min,max;
-    find_minmax(numbers,min,max);
-    const auto bins=make_histogram(numbers,bin_count);
+    Input input ;
+    input=read_input(cin);
+    const auto bins=make_histogram(input);
 
     size_t bin_height;
     show_histogram_svg(bins, bin_height);
